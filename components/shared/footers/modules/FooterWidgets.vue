807abfb5a894b0d9fdf8a823aa1d3ce3112d6715
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-03-17 10:53:45 -0400

    quicklinks, company y business enlaces eliminados del footer

--- components/shared/footers/modules/FooterWidgets.vue
+++ components/shared/footers/modules/FooterWidgets.vue
@@ -35,66 +35,6 @@
                 </ul>
             </div>
         </aside>
-        <aside class="widget widget_footer">
-            <h4 class="widget-title">
-                {{ $t('footer.widgetHeading.quickLink') }}
-            </h4>
-            <ul class="ps-list--link">
-                <li>
-                    <a href="#">Policy</a>
-                </li>
-                <li>
-                    <a href="#">Term & Condition</a>
-                </li>
-                <li>
-                    <a href="#">Shipping</a>
-                </li>
-                <li>
-                    <a href="#">Return</a>
-                </li>
-                <li>
-                    <a href="faqs.html">FAQs</a>
-                </li>
-            </ul>
-        </aside>
-        <aside class="widget widget_footer">
-            <h4 class="widget-title">
-                {{ $t('footer.widgetHeading.company') }}
-            </h4>
-            <ul class="ps-list--link">
-                <li>
-                    <a href="about-us.html">About Us</a>
-                </li>
-                <li>
-                    <a href="#">Affilate</a>
-                </li>
-                <li>
-                    <a href="#">Career</a>
-                </li>
-                <li>
-                    <a href="contact-us.html">Contact</a>
-                </li>
-            </ul>
-        </aside>
-        <aside class="widget widget_footer">
-            <h4 class="widget-title">
-                {{ $t('footer.widgetHeading.bussiness') }}
-            </h4>
-            <ul class="ps-list--link">
-                <li>
-                    <a href="#">Our Press</a>
-                </li>
-                <li>
-                    <a href="checkout.html">Checkout</a>
-                </li>
-                <li>
-                    <a href="my-account.html">My account</a>
-                </li>
-                <li>
-                    <a href="shop-default.html">Shop</a>
-                </li>
-            </ul>
-        </aside>
     </div>
 </template>
 
