285b31962d4613b72adb30bd7ec00e5cf54a0c53
Dave136 <davejs136@gmail.com>
2022-05-28 15:04:08 -0400

    feat: scss support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css: ["~/shared/assets/scss/main.scss"],
   modules: ["~/modules/product/module"],
 });
