a8b7932fc0851594681e772282361ec7f18f0290
Dave136 <davejs136@gmail.com>
2022-05-28 15:00:07 -0400

    feat: add layout file

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <nuxt-layout>
+    <nuxt-page />
+  </nuxt-layout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      Ecommerce App
+      <div>
+        <nuxt-link to="/">Home</nuxt-link> |
+        <nuxt-link to="/product">Product</nuxt-link>
+      </div>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
