fa7cdd7642f72c9ec6fa015a34953efd6be51f26
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-06-11 16:02:51 -0400

    all?

--- components/partials/account/Checkout.vue
+++ components/partials/account/Checkout.vue
@@ -2,7 +2,7 @@
     <div class="ps-checkout ps-section--shopping">
         <div class="container">
             <div class="ps-section__header">
-                <h1>Informacion de facturación</h1>
+                <h1>Informacion de shipping</h1>
             </div>
             <div class="ps-section__content">
                 <div class="ps-form--checkout">
