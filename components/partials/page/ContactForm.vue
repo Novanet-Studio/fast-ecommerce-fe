c0677472988af06b1082c804784651ac90866fb8
Fredy Polania <fpolania@gmail.com>
2022-07-26 17:04:56 -0400

    add: netlify contact form

--- components/partials/page/ContactForm.vue
+++ components/partials/page/ContactForm.vue
@@ -1,61 +1,103 @@
 <template lang="html">
-    <div class="ps-contact-form">
-        <div class="container">
-            <form class="ps-form--contact-us" action="/" method="get">
-                <h3>Envíanos un mensaje</h3>
-                <div class="row">
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
-                        <div class="form-group">
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Nombre *"
-                            />
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
-                        <div class="form-group">
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Email *"
-                            />
-                        </div>
-                    </div>
-                    <div
-                        class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 "
-                    >
-                        <div class="form-group">
-                            <input
-                                class="form-control"
-                                type="text"
-                                placeholder="Asunto *"
-                            />
-                        </div>
-                    </div>
-                    <div
-                        class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 "
-                    >
-                        <div class="form-group">
-                            <textarea
-                                class="form-control"
-                                rows="5"
-                                placeholder="Mensaje"
-                            ></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group submit">
-                    <button class="ps-btn">Enviar mensaje</button>
-                </div>
-            </form>
+  <div class="ps-contact-form">
+    <div class="container">
+      <form
+        class="ps-form--contact-us"
+        name="contacto"
+        v-on:submit.prevent="handleSubmit"
+        action="/gracias/"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <h3>Envíanos un mensaje</h3>
+        <p hidden>
+          <label> Don’t fill this out: <input name="bot-field" /> </label>
+        </p>
+        <div class="row">
+          <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
+            <div class="form-group">
+              <input
+                class="form-control"
+                type="text"
+                name="nombre"
+                v-model="formData.usuario"
+                placeholder="Nombre y Apellido *"
+              />
+            </div>
+          </div>
+          <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
+            <div class="form-group">
+              <input
+                class="form-control"
+                type="email"
+                name="email"
+                v-model="formData.email"
+                placeholder="Email *"
+              />
+            </div>
+          </div>
+          <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
+            <div class="form-group">
+              <input
+                class="form-control"
+                type="text"
+                name="nombre"
+                v-model="formData.asunto"
+                placeholder="Asunto *"
+              />
+            </div>
+          </div>
+          <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
+            <div class="form-group">
+              <textarea
+                class="form-control"
+                rows="5"
+                name="mensaje"
+                v-model="formData.mensaje"
+                placeholder="Mensaje"
+              />
+            </div>
+          </div>
         </div>
+        <div class="form-group submit">
+          <input class="ps-btn" type="submit" value="Enviar" />
+          <input type="hidden" name="form-name" value="inicio" />
+        </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ContactForm'
+  name: "ContactForm",
+  data() {
+    return {
+      formData: {},
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    },
+    handleSubmit(e) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({
+          "form-name": e.target.getAttribute("name"),
+          ...this.formData,
+        }),
+      })
+        .then(() => this.$router.push("/success"))
+        .catch((error) => alert(error));
+    },
+  },
 };
 </script>
 
