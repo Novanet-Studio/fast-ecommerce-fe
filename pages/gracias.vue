f76a08d3e66ea866ead74d03d4915982b6a72440
Fredy Polania <fpolania@gmail.com>
2022-08-04 17:01:58 -0400

    fix: netlify form dummy 1. add: gracias view 2. fix: dummy form structure

---
+++ pages/gracias.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+  <div class="ps-page--single">
+    <bread-crumb :breadcrumb="breadCrumb" />
+    <div class="ps-page--404">
+      <div class="container">
+        <div class="ps-section__content">
+          <figure>
+            <img src="~/static/img/gracias.jpg" alt="" />
+            <h3>¡Gracias por contactarnos!</h3>
+            <p>
+              Le responderemos a la brevedad posible.
+              <nuxt-link to="/">
+                Ir al inicio
+              </nuxt-link>
+            </p>
+          </figure>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BreadCrumb from "~/components/elements/BreadCrumb";
+
+export default {
+  name: "page-404",
+  components: {
+    BreadCrumb,
+  },
+  transition: "zoom",
+  data: () => {
+    return {
+      breadCrumb: [
+        {
+          text: "Inicio",
+          url: "/",
+        },
+        {
+          text: "Gracias",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
