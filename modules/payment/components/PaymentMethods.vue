13f7f0afcd47a1856684a943c678b28337a4b202
Dave136 <davejs136@gmail.com>
2022-11-30 10:48:15 -0400

    feat: add tabs

--- modules/payment/components/PaymentMethods.vue
+++ modules/payment/components/PaymentMethods.vue
@@ -1,23 +1,19 @@
 <template>
   <!-- ps-block--methods -->
-  <div>
-    <!-- TODO: implement tabs for others payments methods -->
-    <div class="w-full">
-      <ul class="flex gap-4 flex-[0_1_auto] relative transition ease whitespace-nowrap">
-        <li v-for="(method, index) in methods" :key="index"
-          class="flex-[1_0_auto] justify-center font-semibold flex cursor-pointer items-center content-center p-3 border text-sm transition ease md:(text-base min-w-[5.625rem] p-4)"
-          :class="[method.active && method.enabled ? 'text-yellow-400 border-yellow-400 hover:(text-yellow-500 border-yellow-500)' : 'text-gray-200 border-gray-200', method.enabled ? 'cursor-pointer' : 'cursor-not-allowed']"
-          :disabled="method.enabled">
-          {{ method.text }}</li>
-      </ul>
-    </div>
-    <div class="py-8">
-      <visa-method />
-    </div>
+  <div class="w-full">
+    <Tabs :options="{ useUrlFragmet: false }">
+      <tab name="Square up">
+        <visa-method />
+      </tab>
+      <tab name="Zelle">
+        Zelle data form goes here...
+      </tab>
+    </Tabs>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Tabs, Tab } from 'vue3-tabs-component';
 type Methods = {
   text: string;
   enabled?: boolean;
@@ -47,3 +43,29 @@ const methods: Methods[] = [
   },
 ]
 </script>
+
+<style scoped>
+:deep(.tabs-component) {
+  /* @apply border; */
+}
+
+:deep(.tabs-component-tabs) {
+  @apply flex gap-4;
+}
+
+:deep(.tabs-component-tab) {
+  @apply p-4 border text-gray-400;
+}
+
+:deep(.is-active:not(a)) {
+  @apply p-4 border border-yellow-500 text-yellow-500;
+}
+
+:deep(.is-disabled:not(a)) {
+  @apply p-4 border border-yellow-500 text-yellow-500;
+}
+
+:deep(.tabs-component-panels) {
+  @apply py-6;
+}
+</style>
