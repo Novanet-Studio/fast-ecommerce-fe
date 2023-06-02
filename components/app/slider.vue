2300ff06279f9c912a556f318ceae29b1e854d8d
David Arenas <davejs136@gmail.com>
2023-06-02 17:05:03 -0400

    add: slider component

---
+++ components/app/slider.vue
@@ -0,0 +1,36 @@
+<template>
+  <swiper
+    :slides-per-view="1"
+    :space-between="1"
+    loop
+    navigation
+    :modules="[Navigation]"
+  >
+    <swiper-slide v-for="(image, index) in slider" :key="index">
+      <nuxt-img
+        class="slider__image"
+        :src="image"
+        alt="Products of the brand"
+      />
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script lang="ts" setup>
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+type Props = {
+  slider?: string[];
+};
+
+defineProps<Props>();
+</script>
+
+<style scoped>
+.slider__image {
+  @apply w-full;
+}
+</style>
