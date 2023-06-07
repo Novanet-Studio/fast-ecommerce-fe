88c62472b140bab5654149b280fb3b095f2f90bb
David Arenas <davejs136@gmail.com>
2023-06-07 18:41:23 -0400

    refactor: nosotros page

---
+++ pages/nosotros.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <nuxt-img src="/img/home/1.jpg" w-full />
+    <div class="py-32 bg-color-3">
+      <div class="w-full py-3 mx-auto lg:max-w-[1230px]">
+        <div class="max-w-[820px] mx-auto mb-12">
+          <h4 class="mb-[65px] text-4xl text-color-2 font-400">Nosotros</h4>
+          <p class="text-xl leading-[1.6em] mb-4 text-[#666]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+            provident repellendus nihil officiis voluptatem quae, qui, obcaecati
+            natus quasi beatae ipsa unde inventore doloremque atque harum illum
+            odio. Reprehenderit repellat doloribus, explicabo soluta inventore
+            consectetur corporis autem voluptatum, obcaecati enim aliquam iste
+            neque pariatur! Dolore neque nesciunt placeat harum autem.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
