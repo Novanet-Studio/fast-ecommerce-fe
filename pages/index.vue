2434740e3efa874aecd0ba17bfaf8c9d3b78fece
David Arenas <davejs136@gmail.com>
2023-08-02 17:43:28 -0400

    refactor: pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,13 @@
+<script lang="ts" setup>
+const { isLoading, categories } = useCategory({ ordered: true });
+</script>
+
 <template>
   <section>
     <app-banner />
     <product-loader v-if="isLoading" />
     <template v-else>
-      <product-landing
-        v-for="category in categories"
-        :key="category.id"
-        :category="category"
-      />
+      <product-landing v-for="category in categories" :key="category.id" :category="category" />
     </template>
   </section>
 </template>
-
-<script lang="ts" setup>
-const { isLoading, categories } = useCategory({ ordered: true });
-</script>
