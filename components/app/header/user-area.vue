e53039bd08be857b9a911736646614a0dddb6f7c
David Arenas <davejs136@gmail.com>
2023-08-04 13:09:57 -0400

    fix(user-area): remove older phosphor icon

--- components/app/header/user-area.vue
+++ components/app/header/user-area.vue
@@ -2,43 +2,27 @@
   <div v-if="!auth.authenticated" class="user-area">
     <div class="user-area__icon-wrapper">
       <nuxt-link to="/login">
-        <ph-user class="user-area__icon" weight="light" />
+        <div class="i-ph-user-light user-area__icon" />
       </nuxt-link>
     </div>
 
     <div class="user-area__links-group">
       <nuxt-link to="/auth/login" class="user-area__link">Ingresar</nuxt-link>
-      <nuxt-link to="/auth/register" class="user-area__link"
-        >Registro</nuxt-link
-      >
+      <nuxt-link to="/auth/register" class="user-area__link">Registro</nuxt-link>
     </div>
   </div>
-  <div
-    v-else
-    class="user-area__menu"
-    @click="isOpen = !isOpen"
-    @mouseover="isOpen = true"
-    @mouseleave="isOpen = false"
-  >
+  <div v-else class="user-area__menu" @click="isOpen = !isOpen" @mouseover="isOpen = true" @mouseleave="isOpen = false">
     <div class="i-ph-user-light user-area__icon" />
     <transition name="slide-fade">
       <div class="user-area__menu-container" v-if="isOpen">
         <ul class="user-area__menu-list">
-          <li
-            v-for="link in global?.links"
-            :key="link.text"
-            class="user-area__menu-list-item"
-          >
+          <li v-for="link in global?.links" :key="link.text" class="user-area__menu-list-item">
             <nuxt-link :to="link.url" class="user-area__menu-link">
               {{ link.text }}
             </nuxt-link>
           </li>
           <li class="user-area__menu-logout">
-            <a
-              href="#"
-              class="transition-colors ease"
-              @click.prevent="handleLogout"
-            >
+            <a href="#" class="transition-colors ease" @click.prevent="handleLogout">
               Cerrar Sesión
             </a>
           </li>
@@ -86,7 +70,7 @@ const handleLogout = () => {
 }
 
 .user-area__menu-container {
-  @apply absolute min-w-[240px] right-0 z-30 pt-[10px] opacity-100 bg-white px-4 py-2 rounded-sm transition ease opacity-100 lg:p-4;
+  @apply absolute min-w-[240px] right-0 z-30 pt-[10px] opacity-100 bg-white px-4 py-2 rounded-sm transition ease opacity-100 lg: p-4;
 }
 
 .user-area__icon {
@@ -98,7 +82,7 @@ const handleLogout = () => {
 }
 
 .user-area__menu-list-item {
-  @apply mb-5 transition hover:(text-yellow-400 font-bold);
+  @apply mb-5 transition hover: (text-yellow-400 font-bold);
 }
 
 .user-area__menu-link {
