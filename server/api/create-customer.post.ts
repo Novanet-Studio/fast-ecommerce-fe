0156b8e050fb3f263fe74185ea0a88a7fa81b8d6
David Arenas <davejs136@gmail.com>
2023-09-01 13:57:06 -0400

    fix(create-customer-api): use native fetch instance of SDK

--- server/api/create-customer.post.ts
+++ server/api/create-customer.post.ts
@@ -1,17 +1,11 @@
-import { Client, Environment } from 'square';
 import type { CreateCustomerRequest, Customer } from 'square';
 
 interface CustomerResponse extends Customer {
   version: bigint;
 }
 
-const { customersApi } = new Client({
-  environment: Environment.Sandbox,
-  accessToken:
-    'EAAAEJyncqd3OPpoDxFbqfbNjmfeDnM_8OZmPxgUfk-ifWbwexuPqMaAUACyfdbs',
-});
-
 export default defineEventHandler(async (event) => {
+  const { square } = useRuntimeConfig();
   try {
     const data = (await readBody(event)) as CreateCustomerRequest;
     const body = {
@@ -19,9 +13,19 @@ export default defineEventHandler(async (event) => {
       givenName: data.givenName,
       emailAddress: data.emailAddress,
     };
-    const response = await customersApi.createCustomer(body);
-    const { version, ...customer } = response.result
-      .customer as CustomerResponse;
+    const response = await $fetch<any>(
+      'https://connect.squareupsandbox.com/v2/customers',
+      {
+        method: 'POST',
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+          'Square-Version': '2023-08-16',
+          Authorization: `Bearer ${square.accessToken}`,
+        },
+      }
+    );
+    const { version, ...customer } = response.customer as CustomerResponse;
 
     return {
       data: customer,
